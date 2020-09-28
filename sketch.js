7ac3bb6590cea35ed1484269e4a814fcd69390bc
J3dI2 <71358519+J3dI2@users.noreply.github.com>
2020-09-27 20:16:37 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//const Engine = Matter.Engine;
+//const World = Matter.World;
+//const Bodies = Matter.Bodies;
+
+//var engine,world,object;
+
+function setup() {
+  var Canvas = createCanvas(400,400);
+
+  //engine = Engine.create();
+  //world = engine.world;
+
+  //object = Bodies.rectangle(200,100,50,50);
+  //World.add(world.object);
+
+//console.log(object);
+} 
+
+function draw() {
+  background(0,0,0);  
+
+  rectMode(CENTER);
+  rect(200,200,50,50);
+
+  
+}
